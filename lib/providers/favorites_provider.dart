861ac179257d627cb87b866ee50f605a6011defb
Marton Urban <urban.marton@gmail.com>
2024-05-20 16:29:51 +0200

    convert StateNotifier to Notifier

--- lib/providers/favorites_provider.dart
+++ lib/providers/favorites_provider.dart
@@ -2,8 +2,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:meals/models/meal.dart';
 
-class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
-  FavoriteMealsNotifier() : super([]);
+class FavoriteMealsNotifier extends Notifier<List<Meal>> {
+  @override
+  List<Meal> build() => [];
 
   bool toggleMealFavoriteStatus(Meal meal) {
     final mealIsFavorite = state.contains(meal);
@@ -19,6 +20,5 @@ class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
 }
 
 final favoriteMealsProvider =
-    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
-  return FavoriteMealsNotifier();
-});
+    NotifierProvider<FavoriteMealsNotifier, List<Meal>>(
+        FavoriteMealsNotifier.new);
